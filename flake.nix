dec8c762d536a0abd6df05e78b936e32871ab9b2
ARKye03 <rafa03-dev@proton.me>
2025-08-25 21:26:09 -0400

    refactor: streamline package creation by reusing mkAstalPkg variable

--- flake.nix
+++ flake.nix
@@ -7,32 +7,44 @@
   in {
     packages = forAllSystems (system: let
       pkgs = nixpkgs.legacyPackages.${system};
+      mkAstalPkg = import ./nix/mkAstalPkg.nix pkgs;
       mkPkg = src:
         import src {
           inherit self pkgs;
-          mkAstalPkg = import ./nix/mkAstalPkg.nix pkgs;
+          mkAstalPkg = mkAstalPkg;
         };
-    in {
-      default = self.packages.${system}.io;
+      
+      packages = {
+        io = mkPkg ./lib/astal/io;
+        astal3 = mkPkg ./lib/astal/gtk3;
+        astal4 = mkPkg ./lib/astal/gtk4;
+        apps = mkPkg ./lib/apps;
+        auth = mkPkg ./lib/auth;
+        battery = mkPkg ./lib/battery;
+        bluetooth = mkPkg ./lib/bluetooth;
+        cava = mkPkg ./lib/cava;
+        greet = mkPkg ./lib/greet;
+        hyprland = mkPkg ./lib/hyprland;
+        mpris = mkPkg ./lib/mpris;
+        network = mkPkg ./lib/network;
+        notifd = mkPkg ./lib/notifd;
+        powerprofiles = mkPkg ./lib/powerprofiles;
+        river = mkPkg ./lib/river;
+        tray = mkPkg ./lib/tray;
+        wireplumber = mkPkg ./lib/wireplumber;
+      };
+    in packages // {
+      default = packages.io;
       docs = import ./docs {inherit self pkgs;};
-
-      io = mkPkg ./lib/astal/io;
-      astal3 = mkPkg ./lib/astal/gtk3;
-      astal4 = mkPkg ./lib/astal/gtk4;
-      apps = mkPkg ./lib/apps;
-      auth = mkPkg ./lib/auth;
-      battery = mkPkg ./lib/battery;
-      bluetooth = mkPkg ./lib/bluetooth;
-      cava = mkPkg ./lib/cava;
-      greet = mkPkg ./lib/greet;
-      hyprland = mkPkg ./lib/hyprland;
-      mpris = mkPkg ./lib/mpris;
-      network = mkPkg ./lib/network;
-      notifd = mkPkg ./lib/notifd;
-      powerprofiles = mkPkg ./lib/powerprofiles;
-      river = mkPkg ./lib/river;
-      tray = mkPkg ./lib/tray;
-      wireplumber = mkPkg ./lib/wireplumber;
+      
+      all-tarballs = pkgs.runCommand "all-astal-tarballs" {
+        nativeBuildInputs = [ pkgs.xz ];
+      } ''
+        mkdir -p $out
+        ${pkgs.lib.concatMapStringsSep "\n" (pkg: ''
+          tar -cJf $out/${pkg.pname}-${pkg.version}.tar.xz -C ${pkg} .
+        '') (builtins.attrValues packages)}
+      '';
     });
 
     devShells = forAllSystems (system:
